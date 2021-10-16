2fcdfca362d2793c955756ac5564fe8bdf5279b7
Shiv <badolashivam@gmail.com>
2021-10-16 23:39:11 +1300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Vapour</title>
+</head>
+<body>
+    
+<!--Navigation Bar-->
+<div class="nav" id="nav">
+  <img src="images/logo.png" class="logo" />
+
+
+
+</div>
+
+<!--Navigation Bar End-->
+
+<!--Search Bar-->
+<div class="search">
+    <input type="text" placeholder="Search"/>
+    <i class="fas fa-search"></i>
+
+
+</div>
+
+<!--Search Bar End-->
+
+
+
+
+
+<!--Games Section-->
+
+
+
+<!--Games Section End-->
+
+</body>
+</html>
