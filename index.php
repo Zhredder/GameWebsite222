164b34c27af649e4bcf64da22d915d072bbf204c
Nathan <nathan.cox.cb@gmail.com>
2021-10-19 00:13:58 +1300

    Add php to home page. Create game page

--- index.php
+++ index.php
@@ -5,57 +5,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vapour - Home</title>
     <link rel="stylesheet" href="css/style.css">
-    <title>Vapour</title>
+    <script defer src="fontawesome/js/all.js"></script>
 </head>
 
 <body>
     <header>
-        <img src="images/logo.png" class="logo">
-        <!--Navigation Bar-->
-        <nav>
-
-        </nav>
-        <!--Navigation Bar End-->
-
-
-        <!--Search Bar-->
-        <div class="search">
-            <input type="text" placeholder="Search">
-            <i class="fas fa-search"></i>
-
-
+        <div class="container">
+            <nav>
+                <img src="images/logo.png" class="logo">
+                <div class="search">
+                    <input type="text" placeholder="Search">
+                    <i class="fas fa-search"></i>
+                </div>
+            </nav>
         </div>
-        <!--Search Bar End-->
     </header>
 
-    <section>
+    <section id="banner">
         <div class="container">
             <div class="slideshow">
                 <div class="slide-container">
-                    <div class="slide" style="background-image: url(images/logo.png);"></div>
-                    <div class="slide" style="background-image: url(images/logo.png);"></div>
-                    <div class="slide" style="background-image: url(images/logo.png);"></div>
+                    <?php
+                    for ($i = 0; $i < 5; $i++) {
+                        echo "<div class='slide' style='background-image: url(images/logo.png);'></div>\n";
+                    }
+                    ?>
+
                 </div>
                 <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
                 <a class="next" onclick="changeSlide(1)">&#10095;</a>
                 <br>
                 <div class="dot-container">
-                    <span class="dot" onclick="setSlide(0)"></span>
-                    <span class="dot" onclick="setSlide(1)"></span>
-                    <span class="dot" onclick="setSlide(2)"></span>
+                    <?php
+                    for ($i = 0; $i < 5; $i++) {
+                        echo "<span class='dot' onclick='setSlide(" . $i . ")'></span>\n";
+                    }
+                    ?>
+
                 </div>
             </div>
         </div>
     </section>
 
+    <section id="games" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <?php
+                for ($i = 0; $i < 20; $i++) {
+                    echo "<div class='col-12 col-md-6 col-lg-4 col-xl-3 game'>\n";
+                    echo "<a href='game.php?id=" . $i . "'>Game</a>\n";
+                    echo "</div>\n";
+                }
+                ?>
+            </div>
+        </div>
+    </section>
 
-    <!--Games Section-->
-
-
-
-    <!--Games Section End-->
-
+    <footer>
+        <div class="container">
+            <p>Footer text</p>
+        </div>
+    </footer>
 
     <script src="js/slideshow.js"></script>
 </body>
