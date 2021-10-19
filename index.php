5b6c9f92d20b228cff370b2f111a1bde9e1249cb
Nathan <nathan.cox.cb@gmail.com>
2021-10-19 20:46:15 +1300

    added xml file. Site reads data from xml

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,27 +29,37 @@
         </div>
     </header>
 
-    <section id="banner">
+    <section id="banner" class="section-padding">
         <div class="container">
             <div class="slideshow">
                 <div class="slide-container">
                     <?php
-                    for ($i = 0; $i < 5; $i++) {
-                        echo "<div class='slide' style='background-image: url(images/logo.png);'></div>\n";
+                    $filename = 'catalog.xml';
+                    if (file_exists($filename)) {
+                        $catalog = simplexml_load_file($filename);
+                        foreach ($catalog->children() as $game) {
+                            echo "<a href='game.php?id=" . $game['id'] . "'>";
+                            if ($game->image) {
+                                echo "<div class='slide' style='background-image: url(" . $game->image . ");'></div>\n";
+                            } else {
+                                echo "<div class='slide' style='background-image: url(images/logo.png);'></div>\n";
+                            }
+                            echo "</a>\n";
+                        }
+                    } else {
+                        exit("Failed to open catalog.xml");
                     }
                     ?>
-
                 </div>
                 <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
                 <a class="next" onclick="changeSlide(1)">&#10095;</a>
                 <br>
                 <div class="dot-container">
                     <?php
-                    for ($i = 0; $i < 5; $i++) {
+                    for ($i = 0; $i < $catalog->count(); $i++) {
                         echo "<span class='dot' onclick='setSlide(" . $i . ")'></span>\n";
                     }
                     ?>
-
                 </div>
             </div>
         </div>
@@ -53,10 +69,16 @@
         <div class="container">
             <div class="row">
                 <?php
-                for ($i = 0; $i < 20; $i++) {
+                foreach ($catalog->children() as $game) {
                     echo "<div class='col-12 col-md-6 col-lg-4 col-xl-3 game'>\n";
-                    echo "<a href='game.php?id=" . $i . "'>Game</a>\n";
-                    echo "</div>\n";
+                    echo "<a href='game.php?id=" . $game['id'] . "' class=''>";
+                    if ($game->image) {
+                        echo "<img src='" . $game->image . "'>";
+                    } else {
+                        echo "<img src='images/logo.png'>";
+                    }
+                    echo "<p>" . $game->title . "</p>";
+                    echo "</a>\n</div>\n";
                 }
                 ?>
             </div>
